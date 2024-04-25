5bc7ac9bdb7798e121b7168c8aedb1edb4f26286
AryaEm <ary4maulanaa@gmail.com>
2024-04-26 05:46:45 +0700

    merapikan div

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const menuicon = document.getElementById('menu-icon');
+const menuicon = document.getElementById('menu-icon'); //
 const menulist = document.getElementById('menu-list');
 
 menuicon.addEventListener('click', () => {
     menulist.classList.toggle('hidden');
+    console.log(menuicon.classList)
 });
 
+const text = document.querySelector(".sec-text")
+
+        const textload = () => {
+            setTimeout(() => { //
+                text.textContent = "Frontend Developer"
+            }, 0)
+            setTimeout(() => {
+                text.textContent = "UI/UX Designer"
+            }, 4000)
+        }
+
+        textload();
+        setInterval(textload, 5000)
